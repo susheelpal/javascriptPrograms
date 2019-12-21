d264f4e0e4f4b528889200a6559939d1f6836bc0
susheelpal <sushilk497@gmail.com>
2019-12-21 19:29:51 +0530

    [Susheel] business logic modified

--- basic_core_programs/flip_coin/flipcoinBL.js
+++ basic_core_programs/flip_coin/flipcoinBL.js
@@ -1,5 +1,5 @@
-var count1 = 0;
-var count0 = 0;
+let count1 = 0;
+let count0 = 0;
 exports.flippedCoinCount = (number) => {
     for (i = 0; i < number; i++) {
         let flipped = Math.round(Math.random() * (1 - 0)) + 0;
@@ -10,14 +10,14 @@ exports.flippedCoinCount = (number) => {
         else
             count0++;
     }
-}
 
-exports.flippedPercentageOf1 = (number) => {
     let flipped1 = count1 / number * 100;
-    return flipped1;
+    console.log("percentage of head flipped", flipped1);
+
+    let flipped0 = count0 / number * 100;
+    console.log("percentage of tail flipped", flipped0);
+
+
 }
 
-exports.flippedPercentageOf0 = (number) => {
-    let flipped0 = count0 / number * 100;
-    return flipped0;
-}
+
--- basic_core_programs/flip_coin/flipcoinmain.js
+++ basic_core_programs/flip_coin/flipcoinmain.js
@@ -1,7 +1,5 @@
 let read = require('readline-sync');
-let callfun = require('./flipcoinBL');
-let number = read.question('tell how many times you want to flip :');
+let callfunction = require('./flipcoinBL');
+let number = read.question('how many times you want to flip :');
 
-callfun.flippedCoinCount(number);
-console.log('percentage of 1 :' + callfun.flippedPercentageOf1(number));
-console.log('percentage of 0 :' + callfun.flippedPercentageOf0(number));
+callfunction.flippedCoinCount(number);
