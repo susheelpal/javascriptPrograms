2e668ae467fcb37e292a592020d8a7bad9e6d229
susheelpal <sushilk497@gmail.com>
2019-12-21 19:33:57 +0530

    [Susheel] file content added

--- basic_core_programs/flip_coin/flipcoinmain.js
+++ basic_core_programs/flip_coin/flipcoinmain.js
@@ -1,3 +1,20 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node flipcoinmain.js
+ *                      2. if nodemon installed cmd> nodemon flipcoinmain.js
+ *
+ *  Purpose         : Print a message with username
+ *
+ *  @description
+ *
+ *  @file           : flipcoinmain.js
+ *  @overview       : read how many times want to flip from console and print percentage of both faces.
+ *  @module         : flipcoinmain - This is optional if expeclictly its an npm or local package
+ *  @author         : Susheel Pal <sushilk497@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 21-12-2019
+ *
+ ******************************************************************************/
+
 let read = require('readline-sync');
 let callfunction = require('./flipcoinBL');
 let number = read.question('how many times you want to flip :');
