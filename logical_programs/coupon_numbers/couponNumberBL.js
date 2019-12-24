ccd34a9f394c74135a27f0175081601be492e833
susheelpal <sushilk497@gmail.com>
2019-12-24 11:12:14 +0530

    [Susheel] business logic has modified

--- logical_programs/coupon_numbers/couponNumberBL.js
+++ logical_programs/coupon_numbers/couponNumberBL.js
@@ -4,12 +4,12 @@
 exports.distinctCoupon = (coupons) => {
     let arr = [];
     for (i = 0; i < coupons; i++) {
-        let cNumber = Math.random();
-        for (j = 0; j < i; j++) {
-            if (cNumber != arr[j]) {
-                arr[j] = cNumber;
-            }
-        }
+        arr[i] = parseInt(Math.random() * ((1000 - 100) + 1) + 100);
+        /* for (j = 0; j < i; j++) {
+             if (cNumber != arr[j]) {
+                 arr[j] = cNumber;
+             }
+         }*/
     }
     console.log(arr);
 }
