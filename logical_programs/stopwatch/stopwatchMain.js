2826f40c0e0a38847638ac92a14ef2a0b47ade83
susheelpal <sushilk497@gmail.com>
2019-12-24 11:21:37 +0530

    [Susheel] file content added

---
+++ logical_programs/stopwatch/stopwatchMain.js
@@ -0,0 +1,17 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node stopwatchMain.js
+ *                      2. if nodemon installed cmd> nodemon stopwatchMain.js
+ *
+ *  Purpose         : find the elapsed time.
+ *
+ *  @description
+ *
+ *  @file           : stopwatchMain.js
+ *  @overview       : take a start time and end time and find the elapsed time between them. 
+ *  @module         : module_name - This is optional if expeclictly its an npm or local package
+ *  @author         : Susheel Pal <sushilk497@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 24-12-2019
+ ******************************************************************************/
+let read = require('readline-sync');
+let callFunction = require('./stopwatchBL');
