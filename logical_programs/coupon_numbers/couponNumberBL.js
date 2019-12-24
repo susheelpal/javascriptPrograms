27e5804283054546076a0484bbea00acd147b49c
susheelpal <sushilk497@gmail.com>
2019-12-24 11:14:03 +0530

    [Susheel] business logic has modified

--- logical_programs/coupon_numbers/couponNumberBL.js
+++ logical_programs/coupon_numbers/couponNumberBL.js
@@ -5,11 +5,6 @@ exports.distinctCoupon = (coupons) => {
     let arr = [];
     for (i = 0; i < coupons; i++) {
         arr[i] = parseInt(Math.random() * ((1000 - 100) + 1) + 100);
-        /* for (j = 0; j < i; j++) {
-             if (cNumber != arr[j]) {
-                 arr[j] = cNumber;
-             }
-         }*/
     }
     console.log(arr);
 }
