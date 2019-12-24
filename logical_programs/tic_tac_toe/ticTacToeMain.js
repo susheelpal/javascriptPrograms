e197af678ada2afcf203dda2afac308a388e838f
susheelpal <sushilk497@gmail.com>
2019-12-24 16:14:10 +0530

    [Susheel] file content added

---
+++ logical_programs/tic_tac_toe/ticTacToeMain.js
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node ticTacToeMain.js
+ *                      2. if nodemon installed cmd> nodemon ticTacToeMain.js
+ *
+ *  Purpose         : Tic Tac Toe game.
+ *
+ *  @description
+ *
+ *  @file           : ticTacToeMain.js
+ *  @overview       : take a X or O as a input and play till win or lose. 
+ *  @module         : module_name - This is optional if expeclictly its an npm or local package
+ *  @author         : Susheel Pal <sushilk497@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 24-12-2019
+ ******************************************************************************/
+let callFunction = require('./ticTacToeBL');
