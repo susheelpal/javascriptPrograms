6508b3e74d29e542fd34dcccad568733322ce2e6
susheelpal <sushilk497@gmail.com>
2019-12-27 11:43:12 +0530

    [Susheel] functional comment and main file modified in flip_coin

--- basic_core_programs/flip_coin/flipcoinBL.js
+++ basic_core_programs/flip_coin/flipcoinBL.js
@@ -1,13 +1,14 @@
+
 /**
- * Flip coin.
  * @module flipcoinBL
+ * @param {int} number - number of times to flip
+ * @param {int} count1 - count to head flipped
+ * @param {int} count0 - count to tail flipped
+ * @param {int} flipped1 - percentage of hesd flipped
+ * @param {int} flipped0 - percentage of tail flipped
  */
 let count1 = 0;
 let count0 = 0;
-    /**
-     * @param {int} number - number of times to flip
-     * print percentage of flipped coin faces
-     */
 exports.flippedCoinCount = (number) => {
     for (i = 0; i < number; i++) {
         let flipped = Math.round(Math.random() * (1 - 0)) + 0;
--- basic_core_programs/flip_coin/flipcoinmain.js
+++ basic_core_programs/flip_coin/flipcoinmain.js
@@ -17,6 +17,7 @@
 
 let read = require('readline-sync');
 let callfunction = require('./flipcoinBL');
+
 try {
     let number = read.question('how many times you want to flip :');
     callfunction.flippedCoinCount(number);
