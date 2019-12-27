77a7a4446e315e5dfa1eaea43800e2a1a8cacce5
susheelpal <sushilk497@gmail.com>
2019-12-27 14:44:40 +0530

    [Susheel] file content,functional comment and file has handled in windchill

--- functional_programs/windchill/windchillMain.js
+++ functional_programs/windchill/windchillMain.js
@@ -4,7 +4,11 @@
  *
  *  Purpose         : print wind chill.
  *
- *  @description
+ *  @description    : Write a program WindChill.java that takes two double command-line arguments t and v and prints the wind chill. 
+ *                    Use Math.pow(a, b) to compute ab. Given the temperature t (in Fahrenheit) and the wind speed v (in miles per hour), 
+ *                    the National Weather Service defines the effective temperature (the wind chill) to be:
+ *                      w=35.74 + 0.6215t + (0.4275t - 35.75)*pow(v,0.16)
+
  *
  *  @file           : windchillMain.js
  *  @overview       : take value of a, b and c and find the quadratic equation.
@@ -13,10 +17,15 @@
  *  @version        : 1.0
  *  @since          : 23-12-2019
  ******************************************************************************/
-// let read = require('readline-sync');
-let callFunction = require('./windchillBL');
-let args = process.argv.slice(2);
-let t = args[0];
-let v = args[1];
 
-callFunction.windChill(t, v);
+let callFunction = require('./windchillBL');
+
+try {
+    let args = process.argv.slice(2);
+    let t = args[0];
+    let v = args[1];
+
+    callFunction.windChill(t, v);
+} catch (err) {
+    console.log(err);
+}
