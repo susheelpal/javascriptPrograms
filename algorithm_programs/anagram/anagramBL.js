4667cc68906882841075c8b34a3af0dbe0c2d301
susheelpal <sushilk497@gmail.com>
2019-12-28 16:23:26 +0530

    [Susheel] main file and business logic of anagram added

---
+++ algorithm_programs/anagram/anagramBL.js
@@ -0,0 +1,22 @@
+/**
+ * @module - it will check for the two string are anagram or not
+ * @param {String} sample1 - string to be checked 
+ * @param {String} sample2 - string to be checked 
+ */
+
+exports.checkAnagram = (sample1, sample2) => {
+    if (sample1.length == sample2.length) {
+        sample1 = sample1.split('').sort().join();
+        sample2 = sample2.split('').sort().join();
+        console.log(sample1);
+        console.log(sample2);
+
+        if (sample1 == sample2) {
+            console.log('its an anagram');
+        } else {
+            console.log('not an anagram');
+        }
+    } else {
+        console.log('not of same length hence not an anagram');
+    }
+}
---
+++ algorithm_programs/anagram/anagramMain.js
@@ -0,0 +1,27 @@
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node anagramMain.js
+ *                      2. if nodemon installed cmd> nodemon anagramMain.js
+ *
+ *  Purpose         : Anagram Detection.
+ *
+ *  @description    : One string is an anagram of another if the second is simply a rearrangement of the first. 
+ *                    For example, 'heart' and 'earth' are anagrams...
+ *
+ *  @file           : anagramMain.js
+ *  @overview       : find the anagram og given String. 
+ *  @module         : module_name - This is optional if expeclictly its an npm or local package
+ *  @author         : Susheel Pal <sushilk497@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 28-12-2019
+ ******************************************************************************/
+
+let readline = require('readline-sync');
+let callFunction = require('./anagramBL');
+try {
+    let sample1 = readline.question('enter first string ');
+    let sample2 = readline.question('enter second string ');
+    callFunction.checkAnagram(sample1, sample2); //calling checkAnagram()
+}
+catch (err) {
+    console.log(err);
+}
