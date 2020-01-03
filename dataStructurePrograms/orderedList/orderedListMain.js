a26bcb862da213f3c3a0542a50780ad07cc2786b
susheelpal <sushilk497@gmail.com>
2020-01-03 13:11:00 +0530

    [Susheel]Refactor. file comment added to orderedList

--- dataStructurePrograms/orderedList/orderedListMain.js
+++ dataStructurePrograms/orderedList/orderedListMain.js
@@ -1,3 +1,23 @@
+
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node orderedListMain.js
+ *                      2. if nodemon installed cmd> nodemon orderedListMain.js
+ *
+ *  Purpose         : create an order list.
+ *
+ *  @description    : Read a List of Numbers from a file and arrange it ascending Order in a Linked List. 
+ *                    Take user input for a number, if found then pop the number out of the list else insert the number 
+ *                    in appropriate position.
+
+ *
+ *  @file           : orderedListMain.js
+ *  @overview       : create an ordered list.
+ *  @module         : module_name - This is optional if expeclictly its an npm or local package
+ *  @author         : Susheel Pal <sushilk497@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 03-01-2020
+ ******************************************************************************/
+
 const input = require('readline-sync');
 let list = require('../unorderedList/unorderedListBL');
 let fs = require('fs');
