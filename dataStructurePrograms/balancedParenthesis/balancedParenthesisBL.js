3f1fb0bc721ae4745c9b5c335ed8943280a68071
susheelpal <sushilk497@gmail.com>
2020-01-03 12:35:06 +0530

    [Susheel]Refactor. functional comment and file comment has added to business logic and main file

--- dataStructurePrograms/balancedParenthesis/balancedParenthesisBL.js
+++ dataStructurePrograms/balancedParenthesis/balancedParenthesisBL.js
@@ -1,5 +1,10 @@
 
 let stackUtil = require('../../utility/stackUtil');
+/**
+ * @module balanceParentheses
+ * @param {string} expr -will take the expression
+ */
+
 exports.balanceParentheses = (expr) => {
     let stack = new stackUtil.stack();
     for (let i = 0; i < expr.length; i++) {
