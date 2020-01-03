4e28fbac15bc204ac4fc685c3dc962755613286c
susheelpal <sushilk497@gmail.com>
2020-01-03 17:46:30 +0530

    [Susheel]Refactor. all the testing programs added

--- test/monthlyPaymentTest.js
+++ test/monthlyPaymentTest.js
@@ -6,5 +6,8 @@ describe("monthly payment", () => {
     it("payment", () => {
         assert.equal(callFunction.monthlyPayment(2000, 5, 2), 87.74277946813653);
     });
+    it("payment1", () => {
+        assert.equal(callFunction.monthlyPayment(3000, 5, 2), 131.6141692022048);
+    });
 
 });
