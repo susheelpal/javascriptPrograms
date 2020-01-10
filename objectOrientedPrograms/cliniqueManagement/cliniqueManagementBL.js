6d6f3cc556863cb3d17360f726cb946347259860
susheelpal <sushilk497@gmail.com>
2020-01-10 19:19:13 +0530

    [Susheel]Refactor. business logic and json file has modied in cliniqueManagement

--- objectOrientedPrograms/cliniqueManagement/cliniqueManagementBL.js
+++ objectOrientedPrograms/cliniqueManagement/cliniqueManagementBL.js
@@ -2,7 +2,12 @@ let fs = require('fs');
 let read = require('readline-sync');
 let details = JSON.parse(fs.readFileSync('cliniqueDetails.json'));
 // console.log(details.doctors[0].Id);
+exports.doctorAppointment = () => {
+    console.log(details.doctors);
+    let id = read.question('Enter doctor\'s ID :');
+    let date = read.question('Enter appointment date :');
 
+}
 for (let i = 0; i < details.doctors.length; i++) {
     if (details.doctors[i].Id === 1236) {
         // console.log(details.doctors[i]);
@@ -12,9 +17,34 @@ for (let i = 0; i < details.doctors.length; i++) {
 
 for (let i = 0; i < details.patients.length; i++) {
     let patient = details.patients[i].name;
-    console.log(patient);
+    // console.log(patient);
 
     if (details.patients[i].Id === 4111) {
         // console.log(details.patients[i]);
     }
+}
+const v = {
+    "person": [
+        {
+            "name": "susheel pal",
+            "city": "bangalore",
+            "state": "karnataka",
+            "pincode": "560098",
+            "mobileNo": 6695654654
+        },
+        {
+            "name": "",
+            "city": "dh",
+            "state": "hd",
+            "pincode": "55465",
+            "mobileNo": 7464
+        },
+        {
+            "name": "sharanya",
+            "city": "banglore",
+            "state": "karnataka",
+            "pincode": "560055",
+            "mobileNo": 7411696506
+        }
+    ]
 }
