be942dc9148fab744612f3e8f6b492499c22fc85
susheelpal <sushilk497@gmail.com>
2020-01-11 15:52:17 +0530

    [Susheel]Refactor. asyncEx.js has modified

--- asyncEx.js
+++ asyncEx.js
@@ -1,12 +1,12 @@
 //example of async and await
 function add(a, b) {
-    return a + b
+    return a + b;
 }
 
 async function fun1(a, b) {
     console.log("additon");
-    let result = await add(a, b)
+    let result = await add(a, b);
     console.log(result);
 
 }
-fun1(10, 20, add)
+fun1(10, 20, add);
