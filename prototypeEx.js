3aa9213b0af1e2cbac4d04e09035617940129647
susheelpal <sushilk497@gmail.com>
2020-01-11 15:44:56 +0530

    []

---
+++ prototypeEx.js
@@ -0,0 +1,24 @@
+//example of prototype
+function student(rollNo, name, age) {
+    this.rollNo = rollNo;
+    this.name = name;
+    this.age = age;
+
+}
+// student.prototype.show = () => {
+//     console.log('hi');
+// }
+student.prototype.show = "hii";
+const s1 = new student(101, 'shre', 21);
+const s2 = new student(100, 'sush', 22);
+// s1.add();
+// console.log(s1.institute);
+
+for (let x in s1) {
+    console.log(s1[x]);
+}
+/*let arr = [1, 2, 3];
+for (let y of arr) {
+    console.log(y);
+}*/
+// console.log(s1)
