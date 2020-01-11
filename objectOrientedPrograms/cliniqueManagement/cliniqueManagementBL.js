ff6ee0c29312f817a54540d20f063ce65e3ac233
susheelpal <sushilk497@gmail.com>
2020-01-11 11:05:05 +0530

    [Susheel]Refactor. business logic and main file has modified in cliniqueManagement

--- objectOrientedPrograms/cliniqueManagement/cliniqueManagementBL.js
+++ objectOrientedPrograms/cliniqueManagement/cliniqueManagementBL.js
@@ -1,50 +1,46 @@
+
 let fs = require('fs');
-let read = require('readline-sync');
 let details = JSON.parse(fs.readFileSync('cliniqueDetails.json'));
-// console.log(details.doctors[0].Id);
-exports.doctorAppointment = () => {
-    console.log(details.doctors);
-    let id = read.question('Enter doctor\'s ID :');
-    let date = read.question('Enter appointment date :');
+/**
+ * @module - this method will search the doctor there in clinique json file using search parameter.
+ * @param {*} search - keyword to search for entries in json file. 
+ */
 
-}
-for (let i = 0; i < details.doctors.length; i++) {
-    if (details.doctors[i].Id === 1236) {
-        // console.log(details.doctors[i]);
-    }
-}
-// console.log(details.doctors);
-
-for (let i = 0; i < details.patients.length; i++) {
-    let patient = details.patients[i].name;
-    // console.log(patient);
-
-    if (details.patients[i].Id === 4111) {
-        // console.log(details.patients[i]);
-    }
-}
-const v = {
-    "person": [
-        {
-            "name": "susheel pal",
-            "city": "bangalore",
-            "state": "karnataka",
-            "pincode": "560098",
-            "mobileNo": 6695654654
-        },
-        {
-            "name": "",
-            "city": "dh",
-            "state": "hd",
-            "pincode": "55465",
-            "mobileNo": 7464
-        },
-        {
-            "name": "sharanya",
-            "city": "banglore",
-            "state": "karnataka",
-            "pincode": "560055",
-            "mobileNo": 7411696506
+searchDoctor = (search) => {
+    for (let i = 0; i < details.doctors.length; i++) {
+        if (details.doctors[i].name == search) {
+            console.log(details.doctors[i]);
         }
-    ]
+        if (details.doctors[i].Id == search) {
+            console.log(details.doctors[i]);
+        }
+        if (details.doctors[i].specialisation == search) {
+            console.log(details.doctors[i]);
+        }
+
+    }
+}
+
+/**
+ * @module - this method will search the patients there in clinique json file using search parameter.
+ * @param {*} search - keyword to search for entries in json file. 
+ */
+
+searchPatient = (search) => {
+    for (let i = 0; i < details.patients.length; i++) {
+        if (details.patients[i].name == search) {
+            console.log(details.patients[i]);
+        }
+        if (details.patients[i].Id == search) {
+            console.log(details.patients[i]);
+        }
+        if (details.patients[i].phoneNumber == search) {
+            console.log(details.patients[i]);
+        }
+
+    }
+}
+
+module.exports = {
+    searchDoctor, searchPatient
 }
--- objectOrientedPrograms/cliniqueManagement/cliniqueManagementMain.js
+++ objectOrientedPrograms/cliniqueManagement/cliniqueManagementMain.js
@@ -1,7 +1,54 @@
-let read = require('readline-sync')
+
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node cliniqueManagementMain.js
+ *                      2. if nodemon installed cmd> nodemon cliniqueManagementMain.js
+ *
+ *  Purpose         : clinique Management.
+ *
+ *  @description    : This programme is used to manage a list of Doctors associated with the Clinique. This also manages 
+ *                    the list of patients who use the clinique. It manages Doctors by Name, Id, Specialization and Availability 
+ *                    (AM,  PM or both). It manages Patients by Name, ID, Mobile Number and Age. The Program allows users to search 
+ *                    Doctor by name, id, Specialization or Availability.
+ *
+ *  @file           : cliniqueManagementMain.js
+ *  @overview       : clinique Management.
+ *  @module         : module_name - This is optional if expeclictly its an npm or local package
+ *  @author         : Susheel Pal <sushilk497@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 10-01-2020
+ ******************************************************************************/
+const readline = require('readline-sync');
 let call = require('./cliniqueManagementBL')
+try {
+    let search = '';
+    for (; ;) {
+        const choice = readline.questionInt(`******Doctor****** \n Doctor by \n 1. Name \n 2. Specialisation \n 3. Id \n ********Patient******* \n Patients by \n 4. Name \n 5. Phone Number \n 6. Id  \n 7. Exit \n Enter the choice to search :`);
+        switch (choice) {
+            case 1: search = readline.question('enter the name of doctor you want to search ');
+                searchDoctor(search);
+                break;
+            case 2: search = readline.question('enter the specialisation of doctor you want to search ');
+                searchDoctor(search);
+                break;
+            case 3: search = readline.questionInt('enter the id of doctor you want to search ');
+                searchDoctor(search);
+                break;
+            case 4: search = readline.question('enter the name of patient you want to search ');
+                searchPatient(search);
+                break;
+            case 5: search = readline.question('enter the phone number of patient you want to search ');
+                searchPatient(search);
+                break;
+            case 6: search = readline.question('enter the Id of patient you want to search ');
+                searchPatient(search);
+                break;
+            case 7: process.exit(0);
 
-console.log('***** Clinique Management *****');
-let id = read.question('Enter doctor\'s ID :');
-let date = read.question('Enter appointment date :');
+            default: console.log('enter valid input');
+                break;
 
+        }
+    }
+} catch (err) {
+    console.log(err);
+}
