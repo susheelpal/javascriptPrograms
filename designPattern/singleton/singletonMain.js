127dfa8b89381ac31b1a94a93525844a6319df08
susheelpal <sushilk497@gmail.com>
2020-01-13 13:10:25 +0530

    [Susheel]Refactor. business logic and main file has modified in singleton

---
+++ designPattern/singleton/singletonMain.js
@@ -0,0 +1,24 @@
+
+/******************************************************************************
+ *  Execution       :   1. default node         cmd> node singletonMain.js
+ *                      2. if nodemon installed cmd> nodemon singletonMain.js
+ *
+ *  Purpose         : example of singleton design pattern
+ *
+ *  @description    : Singleton Design Pattern
+ *
+ *  @file           : singletonMain.js
+ *  @overview       : Singleton design pattern
+ *  @module         : module - This is optional if expeclictly its an npm or local package
+ *  @author         : Susheel Pal <sushilk497@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 13-1-2019
+ *
+ ******************************************************************************/
+let call = require('./singletonBL')
+let s = new call.Singleton('Object1');
+const obj1 = s.getObject();
+const obj2 = s.getObject();
+console.log(obj1)
+console.log(obj2)
+console.log(obj1 == obj2);
